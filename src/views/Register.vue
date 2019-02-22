21b7406f9cb7a6c722a86b9429acf42e0d57f93f
LehruAngela <angelalehru@gmail.com>
2019-02-22 17:43:41 +0300

    Account feature

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,22 +1,22 @@
 <template>
-  <h1>
-    <div className="form-margin">
-      <h1>Yummy Recipes App</h1><br/>
-      <form onSubmit={this.handleRegister}>
+    <div class="form-margin">
+      <navbar/>
+      <h1>Yummy Recipes App</h1>
+      <form onSubmit={this.handleRegister}> 
           <div className="form-group">
             <h4><label htmlFor="email">Email:</label></h4>
             <input name="email" type="email" value={email} onChange={this.handleInputChange}/>
           </div>
           <div className="form-group">
-            <h4><label htmlFor="text">Username:</label><br/></h4>
+            <h4><label htmlFor="text">Username:</label></h4>
             <input name="username" type="text" value={username} onChange={this.handleInputChange}/>
           </div>
           <div className="form-group">
-            <h4><label htmlFor="password">Password:</label><br/></h4>
+            <h4><label htmlFor="password">Password:</label></h4>
             <input name="password" type="password" value={password} onChange={this.handleInputChange}/>
           </div>
           <div className="form-group">
-            <h4><label htmlFor="confirmpassword">Confirm Password:</label><br/></h4>
+            <h4><label htmlFor="confirmpassword">Confirm Password:</label></h4>
             <input name="confirm_password" type="password" value={confirm_password} onChange={this.handleInputChange}/>
           </div>
           <div className="form-group">
@@ -31,5 +31,12 @@
         <p className="copyright text-muted small footer">Copyright © Gela 2019.</p>
       </footer>
     </div>
-  </h1>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  
+}
+</script>
+
